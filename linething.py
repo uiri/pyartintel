3f2a4da2952b6349df8dae018d67c2d61094e589
Uiri Noyb <uiri00@gmail.com>
2012-04-12 21:07:48 -0400

    Let there be paint splotches

--- linething.py
+++ linething.py
@@ -2,9 +2,11 @@ import sys, pygame, random
 pygame.init() 
 
 #create the screen
-winw = 1000
-winh = 500
+paint = True
+winw = 640
+winh = 480
 window = pygame.display.set_mode((winw, winh)) 
+alreadypainted = []
 
 xcoord = random.randint(0,winw)
 ycoord = random.randint(0,winh)
@@ -33,32 +35,44 @@ while 1:
    for event in pygame.event.get(): 
       if event.type == pygame.QUIT: 
          sys.exit(0)
-   #xcoord = random.randint(0,winw)
-   #ycoord = random.randint(0,winh)
-   #color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-   newcolor = []
-   for component in color:
-      toadd = component + random.randint(-10, 10)
-      if toadd > 255:
-         toadd = 255
-      if toadd < 0:
-         toadd = 0
-      newcolor.append(toadd)
-   color = tuple(newcolor)
-   xdiff = random.randint(-300, 300)
-   ydiff = random.randint(-300, 300)
-   width = random.randint(1,10)
-   if xcoord+xdiff > winw:
-      xdiff = winw - xcoord
-   if ycoord+ydiff > winh:
-      ydiff = winh - ycoord
-   if ycoord+ydiff < 0:
-      ydiff = 0 - ycoord
-   if xcoord+xdiff < 0:
-      xdiff = 0 - xcoord
-   pygame.draw.line(window, color, (xcoord+xdiff, ycoord), (xcoord+xdiff, ycoord+ydiff), width)
-   xcoord += xdiff
-   ycoord += ydiff
-   pygame.display.flip()
-   pygame.gfxdraw.bezier(window, ((40,40),(400,400)), ((120,120)), color)
-   pygame.display.flip()
+   if paint:
+      #xcoord = random.randint(0,winw)
+      #ycoord = random.randint(0,winh)
+      #color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+      newcolor = []
+      for component in color:
+         toadd = component + random.randint(-10, 10)
+         if toadd > 255:
+            toadd = 255
+         if toadd < 0:
+            toadd = 0
+         newcolor.append(toadd)
+      color = tuple(newcolor)
+      xdiff = random.randint(-10, 10)
+      ydiff = random.randint(-10, 10)
+      width = random.randint(1,20)
+      if xcoord+xdiff > winw:
+         xdiff = winw - xcoord
+      if ycoord+ydiff > winh:
+         ydiff = winh - ycoord
+      if ycoord+ydiff < 0:
+         ydiff = 0 - ycoord
+      if xcoord+xdiff < 0:
+         xdiff = 0 - xcoord
+      pygame.draw.line(window, color, (xcoord+xdiff, ycoord), (xcoord+xdiff, ycoord+ydiff), width)
+      while (xcoord, ycoord) in alreadypainted:
+         if random.choice(["x", "y"]) == "x":
+            xcoord += 1
+            if xcoord > winw:
+               xcoord = 0
+         else:
+            ycoord += 1
+            if ycoord > winh:
+               ycoord = 0
+      pygame.draw.circle(window, color, (xcoord, ycoord), width)
+      xcoord += xdiff
+      ycoord += ydiff
+      alreadypainted.append((xcoord, ycoord))
+      pygame.display.flip()
+      if len(alreadypainted) == winh*winw:
+         paint = False
