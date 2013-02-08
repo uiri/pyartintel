913c4bbbc289603116b5b3582b00aa26773b803e
Uiri <uiri00@gmail.com>
2013-02-08 17:16:58 -0500

    Change from METHINKS IT IS LIKE A WEASEL to HELLO WORLD

--- ga.py
+++ ga.py
@@ -5,13 +5,13 @@ letterchoices = [" "]
 for letter in ascii_uppercase:
     letterchoices.append(letter)
 randstr = ""
-for i in xrange(28):
+for i in xrange(11):
     randstr += random.choice(letterchoices)
 
 def fitness(string):
-    perfect = "METHINKS IT IS LIKE A WEASEL"
+    perfect = "HELLO WORLD"
     fit = 0
-    for i in xrange(28):
+    for i in xrange(11):
         if string[i] == perfect[i]:
             fit += 1
     return fit
@@ -20,7 +20,7 @@ def evolve(string):
     fit = fitness(string)
     for i in xrange(100):
         newstring = ""
-        for j in xrange(28):
+        for j in xrange(11):
             if random.random() < 0.05:
                 newstring += random.choice(letterchoices)
             else:
@@ -32,12 +32,10 @@ def evolve(string):
 
 randfit = fitness(randstr)
 gencount = 0
-while randfit != 28:
+while randfit != 11:
     randstr = evolve(randstr)
     if fitness(randstr) > randfit:
         randfit = fitness(randstr)
-        if randfit > 15:
-            print randstr
     gencount += 1
 
-print gencount
+print randstr
